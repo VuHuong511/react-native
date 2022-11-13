7126f2046fa9e1ca1ef761833fb164fd9f3b370e
VuHuong511 <vuhuong51101@gmail.com>
2022-11-13 15:31:37 +0700

    2

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ const App = () => {
   const [destination, setDestination] = React.useState("");
   const [isDatePickerVisible, setDatePickerVisibility] = React.useState(false);
   const [dateSelect, setDate] = React.useState("");
+
   const [value, setValue] = React.useState('Yes');
   const [description, setDescription] = React.useState("")
 
@@ -30,21 +31,20 @@ const App = () => {
 
   return (
     <View style={styles.components}>
-      <View style ={header.header}>
+
+      <View style ={styles.header}>
             <View>
-                <Text style ={header.headerText}>EXPENSE APP</Text>
+                <Text style ={styles.headerText}>EXPENSE APP</Text>
             </View>
       </View>
-
+     
       <TextInput
         label="Name"
         style={styles.inputStyle}
         value={name}
         theme={theme}
         mode="outlined"
-        onChangeText={text => setName(text)}
-      
-      />
+        onChangeText={text => setName(text)}/>
 
       <TextInput
         label="Destination"
@@ -54,41 +54,39 @@ const App = () => {
         mode="outlined"
         onChangeText={text => setDestination(text)}
       />
+      <View style = {styles.Date}>
+        <TextInput
+          style={styles.inputDate}
+          value={dateSelect ? dateSelect.toLocaleDateString(): "Date"}
+          theme={theme}
+          mode="outlined"
+          onChangeText={text => setDestination(text)}/>
 
-      <TextInput
-        style={styles.inputStyle}
-        value={dateSelect ? dateSelect.toLocaleDateString(): "Date"}
-        theme={theme}
-        mode="outlined"
-        onChangeText={text => setDestination(text)}
-
-      />
-
-     <View>
-
-
- 
-      <Button  title="Click to choose date" onPress={showDatePicker} />
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode="date"
-        onConfirm={handleConfirm}
-        onCancel={hideDatePicker}
-      />
-    </View>
-      
-
-      <Text>  Require Risks assessment</Text>
-    <RadioButton.Group style={styles.radioButton} onValueChange={newValue => setValue(newValue)} value={value}>
-      <View style={styles.radioButton} >
-        <Text>Yes</Text>
-        <RadioButton value="Yes" />
+        <View style = {styles.buttonDate}>
+          <Button
+          title="Click to choose date" onPress={showDatePicker} />
+          <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}/>
+        </View>
       </View>
-      <View>
-        <Text>No</Text>
-        <RadioButton value="No" />
+
+
+      <Text style = {styles.text}>  Require Risks assessment</Text>
+       <View>
+          <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
+          <View style = {styles.radioButton}>
+            <Text>Yes</Text>
+            <RadioButton value="Yes" />
+          </View>
+          <View style = {styles.radioButton}>
+            <Text>No</Text>
+            <RadioButton value="No" />
+          </View>
+        </RadioButton.Group>
       </View>
-    </RadioButton.Group>
 
 
       <TextInput
@@ -97,24 +95,16 @@ const App = () => {
         value={description}
         theme={theme}
         mode="outlined"
-        onChangeText={text => setDescription(text)}
-      />
-
+        onChangeText={text => setDescription(text)}/>
         <Button  title="Add Trip" onPress={() => console.log('pressed')} />
-    
       </View>
-  )
-
+  );
 }
 
-
-
-
-
- 
-
-
-const header = StyleSheet.create({
+const styles = StyleSheet.create({
+    components:{
+      flex:1,
+    },
     header:{
         width:'100%',
         height:'10%',
@@ -130,33 +120,54 @@ const header = StyleSheet.create({
         letterSpacing: 1,
 
     },
+    inputStyle:{
+      margin:15,
+      paddingRight: 10,
+    },
     radioButton:{
-      padding: 20
+      flexDirection: "row",
+      padding: 10,
+      height: 50,
+      marginTop: 5,
+      marginBottom: 5,
+      marginLeft: 15,
+      marginRight: 200,
+      paddingLeft: 10,
+      paddingRight: 10,
+      alignItems: "center",
+    },
+    radioGroup:{
+      flexDirection: "row",
+      alignItems:'center',   
+    },
+
+    Date:{
+      flexDirection: "row",
+      alignItems:'center',   
     },
     inputDate:{
-      margin:5,
-      paddingRight: 10,
+      margin:15,
+      marginRight:0,
+      paddingRight: 150,
+
+    },
+    buttonDate:{
+      marginLeft:3,
+      marginRight: 15,
+      flex:1,
+      paddingTop: 6,
+    },
+    text:{
+      margin:10,
     }
+
 });
 
 const theme ={
   colors:{
-    primary:"blue"
-    
+    primary:"blue",
   }
-}
-
-const styles = StyleSheet.create({
-  components:{
-    flex:1,
-    
-  },
-  inputStyle:{
-    margin:5,
-    paddingRight: 10,
-    
-  },
-})
+};
 export default App;
 
 
